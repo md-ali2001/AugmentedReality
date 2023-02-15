426cdca4d2c9590400a5478676a0d0308da8f37d
M.A <md.ali2001@hotmail.com>
2023-02-15 06:40:58 -0800

    initialized ar-core

--- app/src/main/java/com/example/augmentedreality/MainActivity.kt
+++ app/src/main/java/com/example/augmentedreality/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.augmentedreality
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.Config
+import com.google.ar.core.Session
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        maybeEnableArButton()
+
+    }
+
+    fun maybeEnableArButton() {
+        val availability = ArCoreApk.getInstance().checkAvailability(this)
+        if (availability.isTransient) {
+            // Continue to query availability at 5Hz while compatibility is checked in the background.
+            Handler().postDelayed({
+                maybeEnableArButton()
+            }, 200)
+        }
+        if (availability.isSupported) {
+            Toast.makeText(this,"yes",Toast.LENGTH_LONG).show()
+        } else { // The device is unsupported or unknown.
+            Toast.makeText(this,"no",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun createSession() {
+        // Create a new ARCore session.
+        val session = Session(this)
+
+        // Create a session config.
+        val config = Config(session)
+
+        // Do feature-specific operations here, such as enabling depth or turning on
+        // support for Augmented Faces.
+
+        // Configure the session.
+        session.configure(config)
     }
 }
